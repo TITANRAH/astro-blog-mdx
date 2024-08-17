42950d782e1025b1999e225fbd071fdcfa76b105
Sergio Miranda <152328184+TitanDevelopMan@users.noreply.github.com>
2024-08-17 11:52:03 -0400

    first commit

---
+++ src/config/site-config.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+    title: 'My awesome blog',
+    description: 'This is my awesome blog',
+    // siteUrl: 'https://my-awesome-blog.com',
+}
---
+++ src/content/config.ts
@@ -0,0 +1,42 @@
+import { defineCollection, reference, z } from "astro:content";
+
+const blogCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+      image: image().refine((img) => img.width < 1200, {
+        message: "Image should be lower than 1200px",
+      }),
+
+      // relacion
+      author: reference("author"),
+
+      // relacion
+      tags: z.array(z.string()),
+
+      //bolean
+      isDraft: z.boolean().default(false),
+    }),
+});
+
+const authorCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      avatar: image(),
+      twitter: z.string(),
+      linkedIn: z.string(),
+      github: z.string(),
+      bio: z.string(),
+      subtitle: z.string(),
+    }),
+});
+
+export const collections = {
+  blog: blogCollection,
+  author: authorCollection,
+};
